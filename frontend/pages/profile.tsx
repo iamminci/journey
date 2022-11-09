8e696b6695dd5f4403797f683b136f8bc514793a
iamminci <0xminci@gmail.com>
2022-11-09 12:55:46 +0000

    ADDED: profile page

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -5,13 +5,76 @@ import {
   Link as ChakraLink,
   VStack,
   HStack,
+  Image,
+  Box,
+  SimpleGrid,
 } from "@chakra-ui/react";
+import Landing from "@components/Landing";
 import { useTron } from "@components/TronProvider";
 import withTransition from "@components/withTransition";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import styles from "@styles/Profile.module.css";
+import { abridgeAddress } from "@utils/abridgeAddress";
+import { FaDiscord, FaGithub, FaTwitter } from "react-icons/fa";
+import RewardPill from "@components/RewardPill";
 
 function Profile() {
+  const { address } = useTron();
+
+  if (!address) return <Landing />;
+
+  return (
+    <VStack pt="6rem">
+      <VStack w="1180px" position="relative">
+        <Image src="/cover.png" alt="cover"></Image>
+        <HStack className={styles.profileContainer}>
+          <HStack w="33%" alignItems="flex-end">
+            <RewardPill imageUrl="/medal.svg" label="Platinum" />
+            <RewardPill imageUrl="/sparkle.svg" label="12000 XP" />
+          </HStack>
+          <VStack w="33%">
+            <Image
+              src="/profile.png"
+              alt="profile"
+              className={styles.profileImage}
+            ></Image>
+            <Text className={styles.profileName}>journeyor</Text>
+            <Text className={styles.profileAddress}>
+              ({abridgeAddress(address)})
+            </Text>
+          </VStack>
+          <HStack w="33%" justifyContent="flex-end" alignItems="flex-end">
+            <VStack className={styles.socialIcon}>
+              <FaDiscord />
+            </VStack>
+            <VStack className={styles.socialIcon}>
+              <FaGithub />
+            </VStack>
+            <VStack className={styles.socialIcon}>
+              <FaTwitter />
+            </VStack>
+          </HStack>
+        </HStack>
+      </VStack>
+      <VStack pt="2rem">
+        <VStack w="100%" alignItems="flex-start">
+          <Text className={styles.profileName}>Badges</Text>
+        </VStack>
+        <SimpleGrid columns={4} gap={5}>
+          {[0, 0, 0, 0].map((num) => (
+            <Box w="300px" key={num}>
+              <Image src="/tronnaut.jpg" borderRadius="20px"></Image>
+            </Box>
+          ))}
+        </SimpleGrid>
+      </VStack>
+    </VStack>
+  );
+}
+
+export default withTransition(Profile);
+
+function TwitterConnect() {
   const { address } = useTron();
   const [fetchedUser, setFetchedUser] = useState();
   const [tweetURL, setTweetURL] = useState("");
@@ -60,14 +123,6 @@ function Profile() {
   function handleURLChange(e) {
     setTweetURL(e.target.value);
   }
-
-  if (!address)
-    return (
-      <VStack pt="4rem">
-        <Text>Please connect wallet to proceed</Text>
-      </VStack>
-    );
-
   return (
     <VStack pt="4rem">
       <HStack>
@@ -89,5 +144,3 @@ function Profile() {
     </VStack>
   );
 }
-
-export default withTransition(Profile);
